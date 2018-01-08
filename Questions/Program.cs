cbfb0947b3df852f6cacc8d86509a3a395073121
Rajekevin <rajekevin@hotmail.fr>
2018-01-08 16:43:38 +0100

    Boucle IF Questionnaire

---
+++ Questions/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Questionnaire.Civilite();
+            Questionnaire.Age();
+        }
+            
+    }
+}
