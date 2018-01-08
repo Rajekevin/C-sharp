67932a042814b4883a440dd902c3811a25072272
Rajekevin <rajekevin@hotmail.fr>
2018-01-08 15:02:01 +0100

    Aire + Perimetre de Rectangle

---
+++ Rectangle/Geometrie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle
+{
+    public class Geometrie
+    {
+
+        public static int PerimetreRectangle(int longueur, int largeur)
+        {
+            //Périmètre P = (L + l) x 2
+            int perimetre = (longueur + largeur) * 2;
+            return perimetre;
+        }
+
+        public static int SurfaceRectangle(int longueur, int largeur)
+        {
+            //Aire = L x l
+            int aire = longueur * largeur;
+            return aire;
+        }
+
+    }
+}
---
+++ Rectangle/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Le Périmètre du Rectangle est de " + Geometrie.PerimetreRectangle(2, 2));
+            Console.WriteLine("L'aire de la surface de Rectangle est de " +Geometrie.SurfaceRectangle(2, 2));
+            Console.ReadLine();
+        }
+    }
+}
