c4ff50c25950984d5f292c7663476cf18be53719
Rajekevin <rajekevin@hotmail.fr>
2018-01-09 10:05:35 +0100

    Add gitignore + premier pas dans les tableau

---
+++ Tableau/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tableau
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Donnez la taille de votre tableau :");
+            int n = int.Parse(Console.ReadLine());
+
+            Tableaux.SommeTab(Tableaux.SaisieTab(n));
+        }
+    }
+}
---
+++ Tableau/Tableaux.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tableau
+{
+   public class Tableaux
+    {
+
+
+        public static int SommeTab(int[] tab)
+        {
+            int sommeTab = 0;
+            for(int i=0; i<tab.Length; i++)
+            {
+             sommeTab = tab[i] + sommeTab;
+            }
+            return sommeTab;
+        }
+
+        public static int[] SaisieTab(int x)
+        {
+            int[] vTab = new int[x];
+
+            for (int i = 0; i< vTab.Length; i++) {
+
+                Console.Write("Donnez l'élèment numéro" + i + ":");
+                vTab[i] = int.Parse(Console.ReadLine());
+            }
+
+            return vTab;
+ 
+        }
+
+    }
+}
