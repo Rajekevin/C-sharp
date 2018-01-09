b7b77f91b5d37ec4690d7cfdd2e10809db19b548
Rajekevin <rajekevin@hotmail.fr>
2018-01-09 10:32:35 +0100

    Liste en C#

---
+++ Liste/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Définition de la liste
+            List<string> jours = new List<string>();
+
+            //pour ajouter Lundi, Mardi, Mercredi
+            jours.Add("lundi");
+            jours.Add("Mardi");
+            jours.Add("Mercredi");
+
+
+            //Pour Afficher une liste
+            foreach (string j in jours)
+            {
+                Console.WriteLine(j);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
