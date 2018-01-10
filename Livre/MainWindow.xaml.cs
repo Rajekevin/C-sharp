423b151cacb265092e9ef75d4bdd7a9dcdbf33b8
Rajekevin <rajekevin@hotmail.fr>
2018-01-11 00:00:35 +0100

    .Net Connection with SQL

---
+++ Livre/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Livre
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnValider_Click(object sender, RoutedEventArgs e)
+        {          
+            //Nom du serveur / nom de la base / mode authentification / compte à utiliser
+            SqlConnection cnx = new SqlConnection(
+                @"Data Source=DESKTOP-7H2T06B\SQLEXPRESS; Initial Catalog=librairie;  integrated security=True"
+                );
+
+            //On teste la connexion
+            try
+            {
+                cnx.Open(); //ouvre la connexion
+                MessageBox.Show("Connexion réussie");
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("YOUPLA ERREUR DE CONNEXION A LA BASE !!!!");
+            }
+
+            //Ajout de nouveau Livre
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "INSERT INTO Livre VALUES ( @txtboxISBN , @txtboxTitre,  @txtResume)";
+            cmd.Connection = cnx;
+
+            cmd.Parameters.AddWithValue("@txtboxISBN", txtboxISBN.Text);
+            cmd.Parameters.AddWithValue("@txtboxTitre", txtboxTitre.Text);
+            cmd.Parameters.AddWithValue("@txtResume",  txtResume.Text);
+
+            //Execution de l'UPDATE
+            SqlDataReader dateReader = cmd.ExecuteReader();
+            MessageBox.Show("Reader executé");
+
+            while (dateReader.Read())
+            {    
+                string titre = (string)dateReader["titre"];
+
+                listViewAffiche.Items.Add(titre);
+            }
+        }
+    }
+}
--- MyWpf/MainWindow.xaml.cs
+++ MyWpf/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,38 @@ namespace MyWpf
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnDB_Click(object sender, RoutedEventArgs e)
+        {
+            //1- créer une chaine de connexion
+            //Nom du serveur / nom de la base / mode authentification / compte à utiliser
+            SqlConnection cnx = new SqlConnection(
+                @"Data Source=DESKTOP-7H2T06B\SQLEXPRESS; Initial Catalog=park;  integrated security=True"
+                );
+
+            cnx.Open(); //ouvre la connexion
+            MessageBox.Show("Connexion réussie");
+
+            //SqlCommand cmd = new SqlCommand();
+
+            //cmd.CommandText("select * from ordinateur");
+
+            /*cmd.Connection = cnx;
+
+            SqlDataReader dateReader = cmd.ExecuteReader();
+            MessageBox.Show("Reader executé");
+            
+
+            while (dateReader.Read())
+            {
+                string desg = (string)dateReader["designation"];
+
+                lstAffichage.Items.Add(desg);
+
+            }*/
+
+
         }
     }
 }
