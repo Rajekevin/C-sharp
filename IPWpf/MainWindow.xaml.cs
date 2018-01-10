f9a40597ef59ef660ff93a49b7f0010dbf2b34f5
Rajekevin <rajekevin@hotmail.fr>
2018-01-10 13:46:08 +0100

    WPF Acces DB .NET

--- IPWpf/MainWindow.xaml.cs
+++ IPWpf/MainWindow.xaml.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.Win32;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Memo
+namespace IPWpf
 {
     /// <summary>
     /// Logique d'interaction pour MainWindow.xaml
@@ -25,12 +23,6 @@ namespace Memo
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-                ListeDesTaches.Items.Add(textboxSaisie.Text);
-                textboxSaisie.Text = "";           
         }
     }
 }
